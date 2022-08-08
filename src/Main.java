cc3f192505add6d6a8c81f06db5bc162bdf15d81
huukhang1512 <huukhang1512@gmail.com>
2022-08-08 12:54:38 +1000

    feat: add Stack Data structure

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
-import DataStructure.Map.*;
 import DataStructure.Heap.*;
+import DataStructure.Stack.Stack;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-//        Map<Integer,Integer> map = new Map<>();
         MinHeap heap = new MinHeap();
         for (int i = 0; i <= 30;i++){
-            heap.insert((int)Math.floor(Math.random()*(10-0+1)+0));
+            heap.insert((int)Math.floor(Math.random()*(10+1)+0));
         }
 
         heap.printHeap();
 
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < 30; i++){
+            stack.push(i);
+        }
 
+        while(!stack.isEmpty()){
+            int item = stack.pop();
+            System.out.println(item);
+        }
     }
 }
